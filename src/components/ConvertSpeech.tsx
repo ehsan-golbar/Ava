b6192252659437f5a9f4df3ca97a60b391c2e1de
Ehsan <1382ehg@gmail.com>
2024-07-22 16:26:38 +0330

    add userType component

--- src/components/ConvertSpeech.tsx
+++ src/components/ConvertSpeech.tsx
@@ -1,7 +1,19 @@
+import userIcon from "../assets/user Icon.png"
+import styles from "./speech.module.css"
+import dropIcon from "../assets/drop Icon.png"
+import "../App.css"
 export default function ConvertSpeech() {
     return (
         <>
-        
+        <div className={styles.userType}>
+            <button className="buttonStyle">
+            <img style={{width: 'fit-content'}} src= {dropIcon} alt="dropIcon" />
+            </button>
+            <p style={{height: 'fit-content', color:'#00BA9F'}}>مهمان</p>
+            
+            <img src={userIcon} alt="userIcon" />
+            
+        </div>
         <p>Speech</p>
         </>
     )
