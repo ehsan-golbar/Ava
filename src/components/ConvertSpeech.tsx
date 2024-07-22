a03971903905dcfd082f83eb450d881b5b72dab6
Ehsan <1382ehg@gmail.com>
2024-07-22 17:06:24 +0330

    add skeleton of speech titles

--- src/components/ConvertSpeech.tsx
+++ src/components/ConvertSpeech.tsx
@@ -5,16 +5,24 @@ import "../App.css"
 export default function ConvertSpeech() {
     return (
         <>
-        <div className={styles.userType}>
-            <button className="buttonStyle">
-            <img style={{width: 'fit-content'}} src= {dropIcon} alt="dropIcon" />
-            </button>
-            <p style={{height: 'fit-content', color:'#00BA9F'}}>مهمان</p>
-            
-            <img src={userIcon} alt="userIcon" />
-            
-        </div>
-        <p>Speech</p>
+            <div className={styles.speechBody}>
+                <div className={styles.speechHead}>
+                    <div className={styles.userType}>
+                        <button className="buttonStyle">
+                            <img style={{ width: 'fit-content' }} src={dropIcon} alt="dropIcon" />
+                        </button>
+                        <p style={{ height: 'fit-content', color: '#00BA9F' }}>مهمان</p>
+
+                        <img src={userIcon} alt="userIcon" />
+
+                    </div>
+                </div>
+                <div className={styles.speechOverview}>
+                    <p className={styles.speechTitle}>تبدیل گفتار به متن</p>
+                    <p className={styles.speechDetails}>آوا با استفاده از هزاران ساعت گفتار با صدای افراد مختلف، زبان فارسی را یادگرفته است و میتواند متن صحبت هار را بنویسد.</p>
+                </div>
+                
+            </div>
         </>
     )
 }
