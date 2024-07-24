b26f42075f34edc62aae2c0d2a4f66ba131a418a
Ehsan <1382ehg@gmail.com>
2024-07-24 23:34:42 +0330

    build the file template for rows

--- src/components/Archive.tsx
+++ src/components/Archive.tsx
@@ -1,7 +1,12 @@
 import styles from "./archive.module.css";
-import "../App.css"
+import "../App.css";
 
-import littleChain from "../assets/little chain Icon.png"
+import downloadIcon from "../assets/download Icon.png"
+import copyIcon from "../assets/copy Icon.png"
+import wordIcon from "../assets/Word icon.png"
+import deleteIcon from "../assets/del Btn.png"
+
+import littleChain from "../assets/little chain Icon.png";
 export default function Archive() {
   return (
     <>
@@ -11,39 +16,63 @@ export default function Archive() {
         </div>
 
         <div className={styles.archiveFiles}>
-
-            <div className={styles.fileItems}>
-
-                <div style={{width : '50%'}}>
-                    <p className={styles.fileItemsTitle}>نام فایل</p>
-                </div>
-
-                <div  style={{width : '15%'}}>
-                    <p className={styles.fileItemsTitle} >تاریخ بارگذاری</p>
-                </div>
-
-
-                <div  style={{width : '15%'}}>
-                    <p className={styles.fileItemsTitle}>نوع  فایل </p>
-                </div>
-
-                <div  style={{width : '20%'}}>   
-                    <p className={styles.fileItemsTitle}>مدت زمان </p>
-                </div>
-
+          <div className={styles.fileItems}>
+            <div style={{ width: "50%" }}>
+              <p className={styles.fileItemsTitle}>نام فایل</p>
             </div>
+
+            <div style={{ width: "10%" }}>
+              <p className={styles.fileItemsTitle}>تاریخ بارگذاری</p>
+            </div>
+
+            <div style={{ width: "10%" }}>
+              <p className={styles.fileItemsTitle}>نوع فایل </p>
+            </div>
+
+            <div style={{ width: "10%" }}>
+              <p className={styles.fileItemsTitle}>مدت زمان </p>
+            </div>
+          </div>
           <ul className="ulStyle">
             <li>
               <div className={styles.fileItem}>
                 <div className={styles.fileName}>
-                    <div className={styles.typeLogo}>
-                        <img src={littleChain} alt="typeLogo" />
-                    </div>
+                  <div className={styles.typeLogo}>
+                    <img src={littleChain} alt="typeLogo" />
+                  </div>
 
-                    <div className={styles.fileDescription}>
-                  <p>  https://irsv.upmusics.com/Downloads/Musics/Sirvan%20K </p>
-                    </div>
+                  <div className={styles.fileDescription}>
+                    <p>
+                      {" "}
+                      https://irsv.upmusics.com/Downloads/Musics/Sirvan%20Khttps://irsv.upmusics.com/Downloads/Musics/Sirvan%20K{" "}
+                    </p>
+                  </div>
+
+
                 </div>
+
+
+                <div className={styles.fileDate}>
+                    <p>1400-08-21</p>
+                  </div>
+
+                  <div className={styles.fileType}>
+                    <p>mp3</p>
+                  </div>
+
+                  <div className={styles.fileTime}>
+                    <p>4:29</p>
+                  </div>
+
+
+
+                  <div className={styles.fileActions}>
+                    <img src={downloadIcon} alt="downloadIcon" />
+                    <img src={wordIcon} alt="wordIcon" />
+                    <img src={copyIcon} alt="copyIcon" />
+                    <img src={deleteIcon} alt="deleteIcon" />
+
+                  </div>
               </div>
             </li>
 
