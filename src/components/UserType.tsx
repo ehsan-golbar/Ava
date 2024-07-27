3cce10168a0c89a499fafbe96dbc08120bc10bbf
Ehsan <1382ehg@gmail.com>
2024-07-27 14:55:30 +0330

    add useer type selected component

--- src/components/UserType.tsx
+++ src/components/UserType.tsx
@@ -2,9 +2,46 @@ import styles from "./speech.module.css";
 import dropIcon from "../assets/drop Icon.png";
 import userIcon from "../assets/user Icon.png";
 
+import logoutIcon from "../assets/logout.png"
+
 export default function UserType() {
   return (
     <>
+      <div className={styles.speechHead}>
+        <div className={styles.userType}>
+          
+
+
+       
+
+
+          <div className={styles.userTypeOne} style={{borderBottom:"0.6px solid #00BA9F"}}>
+          <img src={userIcon} alt="userIcon" />
+            <p className={styles.userTypeItem}>مهمان</p>
+
+            <button className="buttonStyle" style={{marginRight:'0.25rem'}}>
+              <img
+                style={{ width: "fit-content", transform: "scaleY(-1)" }}
+                src={dropIcon}
+                alt="dropIcon"
+              />
+            </button>
+
+
+
+          </div>
+
+
+          <div className={styles.userTypeOne}>
+
+          <img src={logoutIcon} alt="logoutIcon" />
+            <p className={styles.userTypeItem}>خروج</p>
+
+          </div>
+        </div>
+      </div>
+
+      {/*     
       <div className={styles.speechHead}>
         <div className={styles.userType}>
           <img src={userIcon} alt="userIcon" />
@@ -18,7 +55,7 @@ export default function UserType() {
             />
           </button>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
